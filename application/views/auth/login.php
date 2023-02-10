de1a9602ae7cc553bd102fadab3f66054e1e4b3d
DESKTOP-0VVF5DD\User <fredygunawan93@gmail.com>
2023-02-10 17:00:05 +0700

    tampilan form

--- application/views/auth/login.php
+++ application/views/auth/login.php
@@ -1,48 +1,37 @@
-<div class="brand">
-    <div class="backawal">
-        <div class="namaawal"><b>PT. SAHAWARE TEK</b></div>
-    </div>
-    <div class="backakhir">
-        <div class="namaakhir"><b> NOLOGI INDONESIA</b></div>
-    </div>
-</div>
-<div class="row justify-content-center mt-5" style="/* display: flex: ; */ position: relative; top: -1000px;">
+<!DOCTYPE html>
+<!-- Created By CodingLab - www.codinglabweb.com -->
+<html lang="en" dir="ltr">
 
-    <div class="col-lg-4">
+<head>
+    <meta charset="utf-8">
+    <!----<title>Login Form Design | CodeLab</title>---->
+    <link rel="stylesheet" href="<?= base_url('dist/css/styleadmin.css') ?>" />
+</head>
 
-        <div class="card o-hidden border-0 shadow-lg my-5">
-            <div class="card-body p-0">
-                <!-- Nested Row within Card Body -->
-                <div class="row">
-                    <div class="col-lg">
-                        <div class="p-3">
-                            <div class="text-left">
-                                <small>Silahkan</small>
-                                <h1 class="h4 text-gray-900 mb-4">Login</h1>
-                            </div>
-                            <form class="user" method="POST" action="">
-                                <div class="form-group">
-                                    <label for="username"><b>Username</b></label>
-                                    <input type="text" class="form-control form-control-user" id="username" name="username" placeholder="Masukan username">
-                                </div>
-                                <div class="form-group">
-                                    <label for="password"><b>Password</b></label>
-                                    <input type="password" class="form-control form-control-user" id="password" name="password" placeholder="Masukan password">
-                                    <div class="text-right">
-                                        <a class="small" href="">Lupa Password?</a>
-                                    </div>
-                                </div>
-                                <button type="submit" class="btn btn-danger btn-user btn-block">
-                                    Log-in
-                                </button>
-                            </form>
-                            <hr>
-                        </div>
-                    </div>
-                </div>
+<body>
+    <div class="wrapper">
+        <div class="title">Login</div>
+        <form action="#">
+            <div class="field">
+                <input type="text" required>
+                <label></label>
             </div>
-        </div>
-
+            <div class="field">
+                <input type="password" required>
+                <label></label>
+            </div>
+            <div class="content">
+                <div class="checkbox">
+                    <input type="checkbox" id="remember-me">
+                    <label for="remember-me">Remember me</label>
+                </div>
+                <div class="pass-link"><a href="#">Forgot password?</a></div>
+            </div>
+            <div class="field">
+                <input type="submit" value="Login">
+            </div>
+        </form>
     </div>
+</body>
 
-</div>
+</html>
