c29d380738b4812af6225e3bd5f2371b235d6f42
DESKTOP-0VVF5DD\User <fredygunawan93@gmail.com>
2023-02-13 09:24:20 +0700

    login

--- application/views/auth/login.php
+++ application/views/auth/login.php
@@ -10,22 +10,19 @@
 
 <body>
     <div class="wrapper">
-        <div class="title">Login</div>
+        <small>Silahkan</small>
+        <div class="title" style="text-align: left;">Login</div>
         <form action="#">
             <div class="field">
                 <input type="text" required>
-                <label></label>
+                <label>username</label>
             </div>
             <div class="field">
                 <input type="password" required>
-                <label></label>
+                <label>Password</label>
             </div>
-            <div class="content">
-                <div class="checkbox">
-                    <input type="checkbox" id="remember-me">
-                    <label for="remember-me">Remember me</label>
-                </div>
-                <div class="pass-link"><a href="#">Forgot password?</a></div>
+            <div class="field" style="text-align: right;">
+                <div class="pass-link"><a href="#">Lupa Password?</a></div>
             </div>
             <div class="field">
                 <input type="submit" value="Login">
