f0a9a31eafbafa93bed406e788bd72765cac12d9
DESKTOP-0VVF5DD\User <fredygunawan93@gmail.com>
2023-05-05 10:49:53 +0700

    footer lowongan pekerjaan dan detail

--- application/views/Lowonganpekerjaan.php
+++ application/views/Lowonganpekerjaan.php
@@ -11,6 +11,8 @@
     <link rel="stylesheet" href="<?= base_url() ?>plugins/sweetalert2-theme-bootstrap-4/bootstrap-4.min.css" />
     <!-- Toastr -->
     <link rel="stylesheet" href="<?= base_url() ?>plugins/toastr/toastr.min.css" />
+    <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css">
+
 
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous" />
     <title>Tampilan Awal</title>
@@ -29,7 +31,7 @@
     </section>
 
     <div>
-        <h4 style="font-family: Arial, Helvetica, sans-serif; text-align: center; margin-top: 2rem;">Lowongan Tersedia</h4>
+        <h4 style="font-family: Arial, Helvetica, sans-serif; text-align: center; font-size: 30px; margin-top: 2rem;">Lowongan Tersedia</h4>
     </div>
     <div class="container" style="margin: auto; max-width: fit-content; padding: 10px;">
         <div class="row justify-content-center">
@@ -100,40 +102,43 @@
 
 
 
-
     <footer class="footer-distributed">
 
         <div class="footer-left">
-
-            <img src="<?php echo base_url('dist/img/sahaware.jpg'); ?>" width="100px" height="80px" alt="Gambar Latar Belakang"><br><br><br><br><br>
-
-            <p class="footer-company-name">© 2023 Sahaware Indonesia</p>
+            <h3>PT.SAHAWARE TEKNOLOGI <span>INDONESIA</span></h3><br><br>
+            <p class="footer-company-name">Copyright © 2023 <strong>PT.SAHAWARE TEKNOLOGI </strong> INDONESIA</p>
         </div>
 
         <div class="footer-center">
-
             <div>
-                <i style="font-family: Arial, Helvetica, sans-serif;">Information</i><br><br>
-                <p><i class="fas fa-map-marker-alt"></i><a href="https://goo.gl/maps/QP4dvUvw1e3TRaMd9">Jl. Terusan Jakarta Utara, Komplek Daichi No. 69 Antapani - Bandung 40282</a></p><br><br>
-                <p><i class="fas fa-phone-alt"></i><a href="">0811-1244-040</a></p><br><br>
-                <p><i class="fas fa-envelope"></i><a href="">office@sahaware.co.id</a></p>
+                <i class="fa fa-map-marker"></i><a href="https://goo.gl/maps/QP4dvUvw1e3TRaMd9">
+                    <p>Jl. Terusan Jakarta Utara, <span style="margin-right: 50hv;">Komplek Daichi No. 69 Antapani - Bandung 40282</span> </p>
+                </a>
             </div>
 
+            <div>
+                <i class="fa fa-phone"></i>
+                <p>0811-1244-040</p>
+            </div>
+            <div>
+                <i class="fa fa-envelope"></i>
+                <p><a href="fredygunawan93@gmail.com">office@sahaware.co.id</a></p>
+            </div>
         </div>
-
         <div class="footer-right">
-
             <p class="footer-company-about">
-                <span>Layanan Kami</span><br><br>
+                <span>About the company</span>
+                <strong>PT. Sahaware Teknologi Indonesia</strong> We are a software development service company from Bandung, Indonesia. Focuses on developing web and mobile applications by using the latest technology
             </p>
-            <p href="">Custom Applicatiop Development</p><br><br>
-            <p href="">Digital Product Development</p><br><br>
-            <p href="">Software Development Outsourcing</p>
-
+            <div class="footer-icons">
+                <a href="https://web.facebook.com/sahaware.id"><i class="fa fa-facebook"></i></a>
+                <a href="https://www.instagram.com/sahaware.id/"><i class="fa fa-instagram"></i></a>
+                <a href="https://www.linkedin.com/company/sahaware/mycompany/"><i class="fa fa-linkedin"></i></a>
+                <a href="https://twitter.com/sahaware"><i class="fa fa-twitter"></i></a>
+                <a href="https://www.youtube.com/@sahawareindonesia2232/featured"><i class="fa fa-youtube"></i></a>
+            </div>
         </div>
-
     </footer>
-
     <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-Fy6S3B9q64WdZWQUiU+q4/2Lc9npb8tCaSX9FK7E8HnRr0Jz8D6OP9dO5Vg3Q9ct" crossorigin="anonymous"></script>
 
